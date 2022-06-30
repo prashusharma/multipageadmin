6e20156c1c15ac915c3d031ca53cf90b3bb76013
prashusharma <prashukumarsharmassm2000@gmail.com>
2022-06-30 18:43:10 +0530

    again adding corrected dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,66 +1,130 @@
-<div class="title fs-2">
-    Dashboard View
-</div>
-<div class="row mt-5">
-    <?php
-    include './config.php';
-        
-    $active = mysqli_query($conn, "select count(*) as num from partners_detail where status = 1");
-    $active = mysqli_fetch_assoc($active);
-
-    $inactive = mysqli_query($conn, "select count(*) as num from partners_detail where status = 2");
-    $inactive = mysqli_fetch_assoc($inactive);
-
-    $deleted = mysqli_query($conn, "select count(*) as num from partners_detail where status = 3");
-    $deleted = mysqli_fetch_assoc($deleted); 
-    ?>
-
-    <div class="activity-block col-md-4 text-center">
-        <div class="console-panel grid-stack-item-content console-no-header">
-            <div class="console-panel-body align-center">
-                <div class="console-stats">
-                    <h5>Total Active Pages</h5>
-                    <span class="sparklines" data-sparkType="bar" data-sparkBarColor="#6794dc" data-sparkBarWidth="10"
-                        data-sparkHeight="50"><?php echo "0,"; $i=1; while($i<=$active['num']){ echo ",".$i; $i++;}?></span>
-                    <div class="stats-num"><?php echo $active['num']?> </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap 5 link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- google font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- custome css  -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/dashboard.css">
+    <title>Create Account</title>
+</head>
+<body>
+    <!------------------ header section  ------------------------>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-12 col-lg-12">
+                        <div class="header-navigation">
+                            <div class="logo-">
+                                <a href=""><img src="./img/organizein-logo-main-1.png" class="img-fluid" alt="logo"></a>
+                            </div>
+                            <div class="header-icon-div">
+                                <div>
+                                    <a class="icon" href="Contact%20Form.html">
+                                        
+                                        <i class="fa-solid fa-envelope">
+                                            <span>0</span>
+                                        </i>
+                                    
+                                        <span>
+                                            Website Email
+                                        </span>
+                                    </a>
+                                </div>
+                                <div>
+                                  <div class="dropdown">
+                                    <a class="dropdown-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" href="#">
+                                      <i class="fa-solid fa-user"></i>
+                                  
+                                      <span>
+                                          My Account
+                                      </span>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                      <li class="text-start"><a class="dropdown-item" href="dashboard.html">Dashboard</a></li>
+                                      <li class="text-start"><a class="dropdown-item" href="My%20Accoun.html">Change Password</a></li>
+                                      <li class="text-start"><a class="dropdown-item" href="adminlogin.html">Database Details</a></li>
+                                    </ul>
+                                  </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <span class="fs-1 mt-2" style="display: inline-block;">Total Active Pages</span>
-    </div>
+        </header>
+    <!------------------ header section end ------------------------>
 
-    <div class="activity-block col-md-4 text-center">
-        <div class="console-panel grid-stack-item-content console-no-header">
-            <div class="console-panel-body align-center">
-                <div class="console-stats">
-                    <h5>Total Not Active Pages</h5>
-                    <span class="sparklines" data-sparkType="bar" data-sparkBarColor="#eed202" data-sparkBarWidth="10"
-                        data-sparkHeight="50"> <?php echo "0,"; $i=1; while($i<=$inactive['num']){ echo ",".$i; $i++;}?> </span>
-                    <div class="stats-num"><?php echo $inactive['num']?> </div>
+    <section>
+        <div class="container">
+            <div class="row">
+                <?php include'partials/sidebar.php' ?>
+                <div class="col-12 col-md-9 col-lg-9">
+                    <div class="right-site">
+                        <h4 style="margin-bottom: -8px;">Dashboard! </h4>
+                        <span style="font-style:italic; font-size:12px; color: rgba(46, 46, 46, 0.767); font-weight: 600;">Welcome Admin to the dashboard</span>
+                    </div>
+                    <div class="row">
+
+                        <div class="col-12 col-md-4 signle-box-dashboard mb-3">
+                            <div>
+                                <h1>4k</h1>
+                            </div>
+                            <p>Total Pages Created</p>
+                        </div>
+
+                        <div class="col-12 col-md-4 signle-box-dashboard mb-3">
+                            <div>
+                                <h1>53k</h1>
+                            </div>
+                            <p>Total Cities</p>
+                        </div>
+
+                        <div class="col-12 col-md-4 signle-box-dashboard mb-3">
+                            <div>
+                                <h1>153k</h1>
+                            </div>
+                            <p>Total Emails so far</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-12 col-md-12">
+                    <div class="log-out mt-5 mb-3">
+                        <a href="login.html">
+                            <i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+                    </div>
                 </div>
             </div>
+            
         </div>
-        <span class="fs-1 mt-2"  style="display: inline-block;">Not Active Pages</span>
-    </div>
-    <div class="activity-block col-md-4 text-center">
-        <div class="console-panel grid-stack-item-content console-no-header">
-            <div class="console-panel-body align-center">
-                <div class="console-stats">
-                    <h5>Total Deleted Pages</h5>
-                    <span class="sparklines" data-sparkType="bar" data-sparkBarColor="#df4759" data-sparkBarWidth="10"
-                        data-sparkHeight="50"> <?php echo "5,"; $i=1; while($i<=$deleted['num']){ echo ",".$i; $i+=5;}?> </span>
-                    <div class="stats-num"><?php echo $deleted['num']?></div>
-                </div>
-            </div>
-        </div>
-        <span class="fs-1 mt-2" style="display: inline-block;">Deleted</span>
-    </div>
-    <!-- <div class="col-12 mt-5">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d30451.34790154377!2d78.48655964999999!3d17.439673000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1636571699574!5m2!1sen!2sin"
-            width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-        <div class="text-center my-2 fs-1">
-            Cities Locations marked on map
-        </div>
-    </div> -->
-</div>
+    </section>
+    
+
+    <!---------------- footer section start  -------------->
+    <?php include 'partials/footer.php' ?>
+    
+    <!-- bootstrap5 link  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script type="text/javascript">
+
+    let darkMode = document.querySelector('.checkbox-tbh');
+    darkMode.addEventListener('click', function(){
+        darkMode.classList.toggle('active');
+    })
+    let darkModex = document.querySelector('.checkbox-tbhx');
+    darkModex.addEventListener('click', function(){
+        darkModex.classList.toggle('active');
+    })
+</script>
+</body>
+</html>
