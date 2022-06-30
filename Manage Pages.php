725ae6290be85ce7f2b45cbc70137506a99209b2
prashusharma <prashukumarsharmassm2000@gmail.com>
2022-06-30 19:01:33 +0530

    modified all the files

--- Manage Pages.php
+++ Manage Pages.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- bootstrap 5 link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- font awesome link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <!-- google font  -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <!-- custome css  -->
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/contactForms.css">
-
-    <title>Create Account</title>
-</head>
-<body>
+<?php include 'partials/header.php' ?>
     <!------------------ header section  ------------------------>
         <header>
             <div class="container">
@@ -268,8 +248,7 @@
 
     <!---------------- footer section start  -------------->
     <?php include 'partials/footer.php' ?>
-    <!-- bootstrap5 link  -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    
 <script type="text/javascript">
 
     let darkMode = document.querySelector('.checkbox-tbh');
@@ -281,5 +260,3 @@
         darkModex.classList.toggle('active');
     })
 </script>
-</body>
-</html>
--- My Accoun.php
+++ My Accoun.php
@@ -171,8 +171,7 @@
 
     <!---------------- footer section start  -------------->
     <?php include 'partials/footer.php' ?>
-    <!-- bootstrap5 link  -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    
 <script type="text/javascript">
 
     let darkMode = document.querySelector('.checkbox-tbh');
@@ -184,5 +183,3 @@
         darkModex.classList.toggle('active');
     })
 </script>
-</body>
-</html>
