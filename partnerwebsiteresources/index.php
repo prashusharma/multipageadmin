47aae58a505c1123261309a09992d86fbc81e078
Deepak Tiwari <74840542+justdeepaktiwari@users.noreply.github.com>
2022-07-07 03:21:01 +0530

    lkjdks

--- partnerwebsiteresources/index.php
+++ partnerwebsiteresources/index.php
@@ -19,7 +19,7 @@
     $phone = empty($row['partner_phone']) ? "+1 646 679-7250" : $row['partner_phone'];
     $email = empty($row['partner_email']) ? "hi@organizein.com" : $row['partner_email'];
     $linkedin = $row['partner_linkedin'];
-    $current_url = $actual_link. 'multipageadmin/' . str_replace(" ", "-", strtolower(trim($country)))."/".str_replace(" ", "-", strtolower(trim($state)))."/".str_replace(" ", "-", strtolower(trim($city)))."/".str_replace(" ", "-", strtolower(trim($services))).".php";
+    $current_url = $actual_link.'/'. 'multipageadmin/' . str_replace(" ", "-", trim($country))."/".str_replace(" ", "-", trim($state))."/".str_replace(" ", "-", trim($city))."/".str_replace(" ", "-", strtolower(trim($services))).".php";
 
     /*<?php echo $country?>
 <?php echo $state?>
