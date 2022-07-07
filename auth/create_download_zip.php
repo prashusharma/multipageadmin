ef0503529ccb7db35fa6796a6a0cd0f6a7ce54dd
prashusharma <prashukumarsharmassm2000@gmail.com>
2022-07-07 20:48:48 +0530

    added download system

--- auth/create_download_zip.php
+++ auth/create_download_zip.php
@@ -7,15 +7,18 @@ $zip->open($file, ZipArchive::OVERWRITE);
 
 // Stuff with content
 foreach (glob("../$folder/*.php") as $file1) { /* Add appropriate path to read content of zip */
-  // $file1 = str_replace("../$folder", "", $file1);
   $zip->addFile($file1);
 }
 // Close and send to users
 $zip->close();
+ob_clean();
+ob_end_clean();
 header('Content-Type: application/zip');
-header('Content-Length: ' . filesize($file));
+header('Content-Length: ' . filesize($file). "\\n");
 header('Content-Disposition: attachment; filename="file.zip"');
 readfile($file);
-unlink($file); 
+// unlink($file);
+
+exit();
 
 ?>
