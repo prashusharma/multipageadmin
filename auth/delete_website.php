94ad70a7dc02b3f0cd5daa8e787e19b08bf55b86
prashusharma <prashukumarsharmassm2000@gmail.com>
2022-07-14 01:05:58 +0530

    changes in delete

--- auth/delete_website.php
+++ auth/delete_website.php
@@ -9,7 +9,7 @@ $sql = "DELETE FROM `website_pages` WHERE partner_id = '$id'";
 mysqli_query($conn, $sql);
 
 $folder = $_GET["folder"];
-$files = glob("../$folder/*"); // get all file names
+$files = glob("../../$folder/*"); // get all file names
 foreach ($files as $file) { // iterate files
     if (is_file($file)) {
         unlink($file); // delete file
